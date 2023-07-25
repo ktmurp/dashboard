4aabd991e090ae5e88ae03343524926997a57604
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2023-07-25 15:41:57 -0400

    Create getPuzzles.js

---
+++ src/API/getPuzzles.js
@@ -0,0 +1,22 @@
+const getPuzzles = async () => {
+    const request = await fetch(
+        //quad_perctanges
+        `https://collecture.org/api/puzzles`,
+        {
+            method: "GET",
+            headers: {
+                mode: 'no-cors',
+            },
+        }
+    );
+
+    const response = await request.json();
+    if (!response.success) {
+        console.log(response);
+    } else {
+        console.log("success");
+    }
+    return response.data;
+};
+
+export default getPuzzles;
